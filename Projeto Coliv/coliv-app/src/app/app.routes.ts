0be55bfc3b5c110638480d6cdf18543531705833
Fernando Alexandre da Silva <fernando17034@gmail.com>
2026-03-28 22:44:15 -0300

    EEDT-34: implementação interface home anfitrião

--- Projeto Coliv/coliv-app/src/app/app.routes.ts
+++ Projeto Coliv/coliv-app/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { PainelAnfitriaoComponent } from './features/painel-anfitriao/painel-anfitriao.component';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [{
-    path: '', component: Home
-}];
+    path: '', component: Home},
+    {
+    path: 'anfitriao', component: PainelAnfitriaoComponent
+    },
+    {
+        path: 'login', component: Login
+    }
+
+];
