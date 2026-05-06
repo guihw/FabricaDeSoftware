6fd641e28f188c72cfb2af2ba28351862ad8167d
Guilherme Santos <guilhermess2005g@gmail.com>
2026-05-05 22:06:25 -0300

    Criado visual da tela de preferencias

--- Projeto Coliv/coliv-app/src/app/app.routes.ts
+++ Projeto Coliv/coliv-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Despesas } from './despesas/despesas';
 import { Cadastro } from './cadastro/cadastro';
+import { FormPreferencias } from './form-preferencias/form-preferencias';
 
 
 export const routes: Routes = [
   {path: '', component: Home},
   {path: 'despesas', component: Despesas}, 
-  {path: 'cadastro', component: Cadastro}
+  {path: 'cadastro', component: Cadastro},
+  {path: 'preferencias', component: FormPreferencias}
 ];
