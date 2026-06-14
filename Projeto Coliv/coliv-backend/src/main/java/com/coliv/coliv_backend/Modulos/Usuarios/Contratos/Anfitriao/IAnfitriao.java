39e26c8ece78810a6525f769a3fb2d3c7dc68423
Guilherme Santos <guilhermess2005g@gmail.com>
2026-06-13 23:15:57 -0300

    Adicionado DTO de credenciais e metodo de busca por email para anfitrião

--- Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Usuarios/Contratos/Anfitriao/IAnfitriao.java
+++ Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Usuarios/Contratos/Anfitriao/IAnfitriao.java
@@ -3,10 +3,12 @@ package com.coliv.coliv_backend.Modulos.Usuarios.Contratos.Anfitriao;
 import com.coliv.coliv_backend.Modulos.Usuarios.Contratos.UsuarioDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IAnfitriao {
 
     public UsuarioDTO obterUsuario(Long id);
     
     public List<UsuarioDTO> obterListaDeUsuarios();
+    Optional<AnfitriaoCredenciaisDTO> buscarCredenciais(String email);
 }
