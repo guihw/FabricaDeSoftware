854da4ad2477ea905ee83c33704ac36095004cde
Guilherme Santos <guilhermess2005g@gmail.com>
2026-06-20 23:59:21 -0300

    .

--- Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Security/Nucleo/SecurityConfig.java
+++ Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Security/Nucleo/SecurityConfig.java
@@ -59,7 +59,11 @@ public class SecurityConfig {
                                 "/cards/anfitriao/**",
                                 "/recomendacoes/feed/anfitriao/**").hasRole("ANFITRIAO")
 
-                        
+                        // Exclusivo de colega
+                        .requestMatchers("/formularios/preferencias-colega/**",
+                                "/recomendacoes/feed/colega",
+                                "/recomendacoes/feed/colega/**",
+                                "/cards/colega/**").permitAll()
 
                         // Demais rotas exigem apenas estar autenticado (anfitrião ou colega)
                         .anyRequest().authenticated())
