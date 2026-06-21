568539e93eca743de517c2af85fec6948dbef255
Guilherme Santos <guilhermess2005g@gmail.com>
2026-06-20 23:52:46 -0300

    Testando exclusão de rotas em produção para verificar problema do railway

--- Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Security/Nucleo/SecurityConfig.java
+++ Projeto Coliv/coliv-backend/src/main/java/com/coliv/coliv_backend/Modulos/Security/Nucleo/SecurityConfig.java
@@ -59,11 +59,7 @@ public class SecurityConfig {
                                 "/cards/anfitriao/**",
                                 "/recomendacoes/feed/anfitriao/**").hasRole("ANFITRIAO")
 
-                        // Exclusivo de colega
-                        .requestMatchers("/formularios/preferencias-colega/**",
-                                "/recomendacoes/feed/colega",
-                                "/recomendacoes/feed/colega/**",
-                                "/cards/colega/**").hasRole("COLEGA")
+                        
 
                         // Demais rotas exigem apenas estar autenticado (anfitrião ou colega)
                         .anyRequest().authenticated())
